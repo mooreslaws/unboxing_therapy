373ffd6967dc50ef8143b30ae475cdd54d56fd39
odiinnn <msinkevic103@gmail.com>
2023-05-28 14:40:02 +0300

    fixes

--- widget/src/routes/Prizes/index.tsx
+++ widget/src/routes/Prizes/index.tsx
@@ -37,28 +37,31 @@ const PrizesPage = () => {
     <div className={styles.container}>
       <div style={{ height: '143px' }} />
       <div className={styles.wrapper}>
-        {prizesArray.map(prize => (
-          <div className={styles.prize}>
-            <div className={styles.left}>
-              <img src={tagToImage[prize.tag]} style={{ width: '77px', aspectRatio: '1/1' }} />
-              <div style={{ textAlign: 'left' }}>
-                <P className={styles.text}>{tagToName[prize.tag]}</P>
-                <a
-                  style={{ color: '#4DC9AC' }}
-                  href={`https://explorer.testnet.mantle.xyz/tx/${getToken(prize.tag)}`}
-                  target={'_blank'}
-                >
-                  {getToken(prize.tag) ? (
-                    <Fragment>
-                      Your tx:
-                      {startAndEnd(getToken(prize.tag), 6)}
-                    </Fragment>
-                  ) : null}
-                </a>
+        {!userRuffles || !userRuffles[0] || !userRuffles[0].tokens ? <P>You prizes will appear here</P> : null}
+        {prizesArray.map(prize =>
+          getToken(prize.tag) ? (
+            <div className={styles.prize}>
+              <div className={styles.left}>
+                <img src={tagToImage[prize.tag]} style={{ width: '77px', aspectRatio: '1/1' }} />
+                <div style={{ textAlign: 'left' }}>
+                  <P className={styles.text}>{tagToName[prize.tag]}</P>
+                  <a
+                    style={{ color: '#4DC9AC' }}
+                    href={`https://explorer.testnet.mantle.xyz/tx/${getToken(prize.tag)}`}
+                    target={'_blank'}
+                  >
+                    {getToken(prize.tag) ? (
+                      <Fragment>
+                        Your tx:
+                        {startAndEnd(getToken(prize.tag), 6)}
+                      </Fragment>
+                    ) : null}
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ) : null,
+        )}
       </div>
     </div>
   );
